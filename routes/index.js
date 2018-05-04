1b6fc077739015639f39d84c240ea2374953c09a
Rein Bulaong <rein.bulaong@yahoo.com>
2018-05-04 18:57:45 +0800

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+const data = require("../data/classpex.json");
+var router = express.Router();
+
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  const heroString = req.query["class"];
+  const aspectString = req.query.aspect;
+  const symbolSize = req.query.symbol;
+  const stroke = req.query.stroke;
+
+  const hero = data.hero[heroString] || data.hero.default;
+  const aspect = data.aspect[aspectString] || data.aspect.default;
+  const title = `${hero.name}${aspect.name ? ` of ${aspect.name}` : ``}`
+
+  const symbolTransform = symbolSize === "big" ? "matrix(1.409 0 0 1.409 -20.859 -20.859)" : undefined;
+  const strokeWidth = stroke === 'thick' ? 1 : stroke === 'thin' ? 0.5 : 0;
+  const symbolStrokeWidth = strokeWidth * ( symbolSize === "big" ? 0.7097 : 1 );
+
+
+  res.setHeader('Content-Type', 'image/svg+xml');
+  res.render('index', { hero, aspect, title, strokeWidth, symbolTransform, symbolStrokeWidth });
+});
+
+module.exports = router;
