095a2f6dac8f3009078a9298234905b4bac79a41
John Reinier Bulaong <rein.bulaong@yahoo.com>
2018-05-08 18:48:45 +0800

    add a copyable html string

--- public/form/generate.js
+++ public/form/generate.js
@@ -2,6 +2,12 @@ function createQueryString(map) {
 	return Object.keys(map).map(function (key) { return "" + key + "=" + map[key] }).join("&")
 }
 
+function setTextAreaContents(queryString="") {
+	var textarea = document.querySelector(".classpex-src");
+		console.log(queryString, textarea);
+	textarea.innerText = "<img src='" + window.location.origin + queryString + "' />'";
+}
+
 document.querySelector("#classpex-form").addEventListener("submit", function(e){
 	e.preventDefault();
 	var heroValue = document.querySelector("#hero-field").value;
@@ -10,4 +16,12 @@ document.querySelector("#classpex-form").addEventListener("submit", function(e){
 	var symbolValue = document.querySelector("[name='symbol']:checked").value;
 	var src = "/?" + createQueryString({"class": heroValue, aspect: aspectValue, stroke: strokeValue, symbol: symbolValue})
 	document.querySelector("#classpex-image").src = src;
+	setTextAreaContents(src);
 })
+
+document.querySelector(".classpex-src").addEventListener("click", function(e) {
+	this.focus();
+	this.select();
+})
+
+setTextAreaContents();
